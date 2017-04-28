6eabbbab65250339dea23e00f1ab1d588e695cdc
Benjamin Acevedo <benjaminacevedo@Benjamins-MacBook-Pro-2.local>
2017-04-27 22:28:47 -0700

    Add twilio routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,6 @@ Rails.application.routes.draw do
  
   root 'users#index'
 
-post '/recieve_sms' do
-  content_type 'text/xml'
-
-  response = Twilio::TwiMl::Response.new do |r|
-    r.Message 'FLAPJACKS MOFO'
-  end
-  return response.to_xml
-end
+  post 'twilio/sms' => 'twilio#sms'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
